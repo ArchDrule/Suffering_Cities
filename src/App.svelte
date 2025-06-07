a9f392e3a9e5be4d6739232b38bd3c23566249df
Георгий <georg.plotnikov03@gmail.com>
2025-06-07 12:36:24 +0500

    Добавил основной блок на странице

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
     import logo from "@/assets/logo.png";
-    import vignette from "@/assets/vignette-0.png";
-    import circleVignette from "@/assets/circle-vignette.png";
+    import vignette from "@/assets/vignettes/vignette-0.png";
+    import headerVignette from "@/assets/vignettes/header-vignette.png";
+    import circleVignette from "@/assets/vignettes/circle-vignette.png";
+    import banner from "@/assets/banner.png";
 
-    import bigCloud from "@/assets/cloud-big.svg";
-    import smallCloud from "@/assets/cloud-small.svg";
-    import doubleCloud from "@/assets/cloud-double.svg";
+    import bigCloud from "@/assets/clouds/cloud-big.svg";
+    import smallCloud from "@/assets/clouds/cloud-small.svg";
+    import doubleCloud from "@/assets/clouds/cloud-double.svg";
 </script>
 
 <main>
@@ -28,19 +30,21 @@
         class="absolute left-1/5 top-[332px] opacity-60"
     />
 
-    <img src={logo} alt="logo" class="w-[660px] mt-8" />
+    <!-- Лого -->
+    <img src={logo} alt="logo" class="w-[660px] mt-4" />
 
+    <!-- Навбар -->
     <div class="flex justify-center mt-6">
         <img
             src={vignette}
             alt="vignette"
-            class="absolute h-12 -translate-y-9 opacity-30"
+            class="absolute w-[276px] -translate-y-9 opacity-30"
         />
-        <nav class="w-[474px] h-20 p-1.5 rounded-lg nav-border-bg z-10">
+        <nav class="w-[474px] h-20 p-1.5 rounded-lg cont-border-bg z-10">
             <div
                 class="w-full h-full px-6 rounded-md nav-bg
                 outline-2 outline-[#260D04] -outline-offset-[5px]
-              flex justify-between items-center"
+                flex justify-between items-center"
             >
                 <!-- Генератор -->
                 <div class="flex justify-center items-center">
@@ -54,7 +58,7 @@
                     </h2>
                 </div>
 
-                <!-- Генератор -->
+                <!-- Сохраненное -->
                 <div class="flex justify-center items-center">
                     <img
                         src={circleVignette}
@@ -64,7 +68,7 @@
                     <h2 class="text-shd cursor-pointer z-10">Сохраненное</h2>
                 </div>
 
-                <!-- Генератор -->
+                <!-- Настройки -->
                 <div class="flex justify-center items-center">
                     <img
                         src={circleVignette}
@@ -76,18 +80,65 @@
             </div>
         </nav>
     </div>
+
+    <!-- Основной контейнер -->
+    <div
+        class="w-[1258px] h-[388px] p-1.5 mt-16 rounded-lg cont-border-bg z-10"
+    >
+        <div
+            class="w-full h-full px-6 rounded-md main-cont-bg
+                outline-2 outline-[#260D04] -outline-offset-[5px]
+                flex flex-col items-center"
+        >
+            <img src={banner} alt="banner" class="w-[900px] -translate-y-10" />
+
+            <h1 class="absolute -translate-y-4">Тупо-Хулипятово</h1>
+
+            <img
+                src={headerVignette}
+                alt="header-vignette"
+                class="absolute w-[344px] mt-14 -scale-100"
+            />
+
+            <button
+                class="w-[280px] h-16 p-1 mt-auto translate-y-1/2 rounded-full button-border-bg cursor-pointer"
+            >
+                <div
+                    class="w-full h-full p-4.5 rounded-full bg-[#D33232]
+                    outline-2 outline-[#260D04] -outline-offset-[4px]
+                    flex justify-between items-center"
+                >
+                    <h2 class="-translate-y-[1px]">⦿</h2>
+                    <h2 class="translate-y-0.5">Сгенерировать</h2>
+                    <h2 class="-translate-y-[1px]">⦿</h2>
+                </div>
+            </button>
+        </div>
+    </div>
 </main>
 
 <style>
+    .cont-border-bg {
+        background-image: url(@/assets/container-border-texture.jpg);
+        background-size: 620px;
+        background-position: center;
+    }
+
     .nav-bg {
         background-image: url(@/assets/wood-table-texture.jpg);
-        background-size: 200%;
+        background-size: 920px;
         background-position: top center;
     }
 
-    .nav-border-bg {
+    .main-cont-bg {
+        background-image: url(@/assets/wood-table-texture.jpg);
+        background-size: 1240px;
+        background-position: center;
+    }
+
+    .button-border-bg {
         background-image: url(@/assets/button-border-texture.jpg);
-        background-size: 130%;
+        background-size: 264px;
         background-position: center;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { mount } from "svelte";
+
 import "./style/output.css";
 import App from "./App.svelte";
 
